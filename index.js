1921bb5cbb4e0ac3e8f506a285b7e297eade85f1
Md. Kamrul Hasan Raza <64928612+infinite-enigma@users.noreply.github.com>
2024-10-16 03:55:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
 console.log(randomNumber1);
 console.log(randomNumber2);
 
+// var randomDiceImage = "dice" + randomNumber1 + ".png";       // dicel.png - dice6.png
+// var randomlmageSource = "images/" + randomDiceImage;         //images/dice1.png - images/dice6.png
+
+/*
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+let randomNumber1 = generateRandomNumber();
+let randomNumber2 = generateRandomNumber();
+
+console.log(randomNumber1, randomNumber2);
+*/
+
 var heading = document.querySelector("h1");
 if (randomNumber1 > randomNumber2) {
   heading.innerHTML = "🚩Player 1 Won!";
